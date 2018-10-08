9cfde8e2f2f715c9ee88b768e664f7117ecae7ea
kingreyvie <43531623+kingreyvie@users.noreply.github.com>
2018-10-08 15:12:37 +0800

    Add files via upload

---
+++ src/pages/home/home.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {Flashlight} from '@ionic-native/flashlight';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  isOn: boolean=true;
+  constructor(private flashlight: Flashlight, public navCtrl: NavController) {
+
+  }
+
+  async isAvailable():Promise<boolean>
+  {
+  try{
+    return await this.flashlight.available();
+  }catch(e){
+    console.log(e);
+  }
+  }
+/*
+Toggle the flashlight to an on or off state
+Get whether the flashligh is Availableif it is availalbe
+toggle the Flashlighttoggle the 'isOn' variable
+If it isnt
+log out to the console
+*/
+  async toggleFlash():Promise<void>{
+    try{
+      let available = await this.isAvailable();
+      if(available){
+        await this.flashlight.toggle();
+        this.isOn = !this.isOn;
+      }else{
+        console.log("Isn't Available.");
+      }
+    }catch(e){
+        console.log(e);
+      }
+  }
+async turnOnFlash(): Promise<void> {
+  await this.flashlight.switchOn();
+}
+async turnOffFlash(): Promise<void>{
+  await this.flashlight.switchOff();
+}
+async isFlashOn(): Promise<boolean>{
+  return await this.flashlight.isSwitchedOn();
+}
+  }
